dfabe1fc82bfcb03633fbec33a597e7503b88cbf
Kane Kenny (s5512001) <s5512001@bournemouth.ac.uk>
2024-10-17 13:02:21 +0100

    defaort

--- src/thegrill/Transform.h
+++ src/thegrill/Transform.h
@@ -5,8 +5,8 @@
 namespace thegrill {
 	struct Transform : Component
 	{
-		glm::vec3 m_position;
-		glm::vec3 m_rotation;
+		glm::vec3 m_position = { 0.0,0.0,0.0 };
+		glm::vec3 m_rotation = { 0.0,0.0,0.0 };
 		glm::vec3 m_scale = { 1.0,1.0,1.0 };
 	};
 }
