5a4e3862d1b9056cbe14fdf36295813008577bb3
Kane Kenny (s5512001) <s5512001@bournemouth.ac.uk>
2024-12-07 18:10:43 +0000

    Rigidbody stuff

--- src/thegrill/RigidBody.cpp
+++ src/thegrill/RigidBody.cpp
@@ -1,7 +1,21 @@
 #include "RigidBody.h"
+#include "BoxCollider.h"
+#include "Core.h"
+#include "Entity.h"
+#include <vector>
+
 
 namespace thegrill {
 	void RigidBody::on_tick() {
-		
+		//get a list of all box colliders using Core::find
+		//for each box collider, check if it is colliding with this rigid body
+//if it is, apply a force to the rigid body
+
+		std::vector<std::shared_ptr<BoxCollider> > colliders;
+		get_entity()->get_core()->find<BoxCollider>(colliders);
+
+		for (auto& collider : colliders) {
+			
+		}
 	}
 }
